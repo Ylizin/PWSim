039f85111657c2f8484b3125f4205abd9d9df018
ylizin <921106097@qq.com>
2021-12-28 00:04:52 +0800

    update

--- experiment/data_utils/dataloader.py
+++ experiment/data_utils/dataloader.py
@@ -89,13 +89,14 @@ class TagDataSet(Dataset):
         self.tag_ids = self.ori_df.iloc[:,3].apply(parse_list)
 #         self.tag_df = self.ori_df.iloc[:,0].apply(d2i) # use enhanced 
         # 0 enhanced , 1 not enhanced
-        self.raw_ids = self.ori_df.iloc[:,0].apply(d2i)  
+        # self.raw_ids = self.ori_df.iloc[:,0].apply(d2i)  
+        self.raw_ids = self.ori_df.iloc[:,4].str.cat(self.ori_df.iloc[:,5],sep = ' ').apply(d2i)  
     
         # self.main_ids = self.ori_df.iloc[:,5].apply(lambda x:[(x,1)])
           
-                                                                    #2 for ext 0 for enrich
+                                                                #2 for ext 0 for enrich
         # self.ext_df = self.ori_df.iloc[:,2].str.cat(self.ori_df.iloc[:,5],sep = ' ').apply(d2b)
-        self.ext_df = self.ori_df.iloc[:,5].apply(d2b)
+        self.ext_df = self.ori_df.iloc[:,2].apply(d2b)
 
         self.chunk_ids = self.ori_df.iloc[:,4].apply(d2i)
         self.goal_ids = self.ori_df.iloc[:,5].apply(d2i)
