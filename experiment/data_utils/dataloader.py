a1221cfd69425b58f906df0bf83819f8ed383ea3
ylizin <921106097@qq.com>
2022-04-09 10:27:34 +0800

    update

--- experiment/data_utils/dataloader.py
+++ experiment/data_utils/dataloader.py
@@ -98,7 +98,7 @@ class TagDataSet(Dataset):
           
                                                                 #2 for ext 0 for enrich
         # self.ext_df = self.ori_df.iloc[:,2].str.cat(self.ori_df.iloc[:,5],sep = ' ').apply(d2b)
-        self.ext_df = self.ori_df.iloc[:,1].apply(d2b)
+        self.ext_df = self.ori_df.iloc[:,2].apply(d2b)
 
         self.chunk_ids = self.ori_df.iloc[:,4].apply(d2i)
         self.goal_ids = self.ori_df.iloc[:,5].apply(d2i)
